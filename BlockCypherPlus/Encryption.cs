421d4107d3fac3d5947c7c5f050765e378895693
Di Saber <marc.a.benamou@gmail.com>
2022-10-28 11:09:59 -0500

    Breaking Changes Changed AES IV generation to be random for every message for security Messages are now a Base64 string of the SharedMessageContainer class that contains the cipher text and the random IV

--- BlockCypherPlus/Encryption.cs
+++ BlockCypherPlus/Encryption.cs
@@ -2,11 +2,21 @@
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace BlockCypherPlus
 {
     internal static class Encryption
     {
+        private static readonly RandomNumberGenerator rng = RandomNumberGenerator.Create();
+
+        public static byte[] RandomIV()
+        {
+            byte[] iv = new byte[16];
+            rng.GetBytes(iv);
+            return iv;
+        }
+
         public static string Encrypt(string input, string key)
         {
             byte[] clearBytes = Encoding.Unicode.GetBytes(input);
@@ -60,12 +70,13 @@ namespace BlockCypherPlus
         {
             byte[] clearBytes = Encoding.Unicode.GetBytes(input);
             byte[] bytes = Encoding.Unicode.GetBytes("70BDGYcJz|xb%31#0n#QizwDq$krwRwUVwUiG!BfiyN!7KofNq");
+            byte[] iv = RandomIV();
 
             using (Aes encryptor = Aes.Create())
             {
                 Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(sharedSecret, bytes, 10000);
                 encryptor.Key = pdb.GetBytes(32);
-                encryptor.IV = pdb.GetBytes(16);
+                encryptor.IV = iv;
 
                 using (MemoryStream ms = new MemoryStream())
                 {
@@ -77,20 +88,26 @@ namespace BlockCypherPlus
                     input = Convert.ToBase64String(ms.ToArray());
                 }
             }
-            return input;
+            return Convert.ToBase64String(Encoding.Unicode.GetBytes(JsonConvert.SerializeObject(new SharedMessageContainer(input, iv))));
         }
 
         public static string Decrypt(string input, byte[] sharedSecret)
         {
             input = input.Replace(" ", "+");
-            byte[] cipherBytes = Convert.FromBase64String(input);
+            //byte[] cipherBytes = Convert.FromBase64String(input);
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+            SharedMessageContainer messageContainer = JsonConvert.DeserializeObject<SharedMessageContainer>(Encoding.Unicode.GetString(Convert.FromBase64String(input)));
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            byte[] cipherBytes = Convert.FromBase64String(messageContainer.Ciphertext);
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
             byte[] bytes = Encoding.Unicode.GetBytes("70BDGYcJz|xb%31#0n#QizwDq$krwRwUVwUiG!BfiyN!7KofNq");
 
             using (Aes encryptor = Aes.Create())
             {
                 Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(sharedSecret, bytes, 10000);
                 encryptor.Key = pdb.GetBytes(32);
-                encryptor.IV = pdb.GetBytes(16);
+                encryptor.IV = messageContainer.IV;
 
                 using (MemoryStream ms = new MemoryStream())
                 {
---
+++ BlockCypherPlus/SharedMessageContainer.cs
@@ -0,0 +1,14 @@
+﻿namespace BlockCypherPlus
+{
+    internal class SharedMessageContainer
+    {
+        public string Ciphertext { get; }
+        public byte[] IV { get; }
+
+        public SharedMessageContainer(string ciphertext, byte[] iv)
+        {
+            Ciphertext = ciphertext;
+            IV = iv;
+        }
+    }
+}
